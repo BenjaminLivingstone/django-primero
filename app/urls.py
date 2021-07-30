2794984d0d3acaef8e1faebfecbe9577cd0b1cca
BenjaminLivingstone <benjaminlivingstone@gmail.com>
2021-07-29 21:02:55 -0400

    primer_proyecto_django OK

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path     
 from . import views
-urlpatterns = [
-    path('', views.index),	  
-    path('hola/', views.hola),	 
+urlpatterns = [	  
+    path('root/', views.root), 
+    path('blogs/', views.index),
+    path('blogs/new', views.new),
+    path('blogs/create', views.create),
+    path('blogs/<number>', views.show),
+    path('blogs/<number>/edit', views.edit),
+    path('blogs/<number>/delete', views.destroy),
+    path('json/', views.json),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse, redirect
+from django.http import JsonResponse
+
+def root(request):
+	return redirect("/blogs")
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
-def hola(request):
-    return HttpResponse("hola")    
+    return HttpResponse("placeholder para luego mostrar una lista de todos los blogs")
+def new(request):
+    return HttpResponse("placeholder para mostrar un nuevo formulario para crear un nuevo blog")
+def create(request):
+	return redirect("/")
+def show(request, number):
+    return HttpResponse(f"placeholder para mostrar el blog numero: {number}")
+def edit(request, number):
+    return HttpResponse(f"placeholder para editar el blog {number}")
+def destroy(request, number):
+	return redirect("/blogs")
+def json(request):
+    return JsonResponse({"nombre":"benjamin", "apellido":"Livingstone"})
+
